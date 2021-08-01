9b8ae250ca2a4349c4c2163ca8f1050f2d5483a5
hannasukharieva <annsukhareva33@gmail.com>
2021-08-01 20:58:21 +0100

    Lodash library

---
+++ Lodash/script.js
@@ -0,0 +1,36 @@
+const _ = {
+  clamp: function (number, lower, upper) {
+    let lowerClampedValue = Math.max(number, lower);
+    let clampedValue = Math.min(lowerClampedValue, upper);
+    return clampedValue;
+  },
+  inRange: function (number, start, end) {
+    if (end === undefined) {
+      end = start;
+      start = 0;
+    } else if (start > end) {
+      let temp = end;
+      end = start;
+      start = temp;
+    }
+    let isInRange = Boolean(start <= number && number < end)
+    return isInRange
+  },
+  words: function (string) {
+    let words = string.split(' ')
+    return words
+  },
+  pad: function(string, length) {
+    if (length <= string.length) {
+      return string
+    } else {
+      let startPaddingLength = Math.floor((string.length-length)/2)
+      let endPaddingLength = Math.floor(length-string.length-startPaddingLength)
+      let paddedString = (' '.repeat(startPaddingLength) + string + ' '.repeat(3))
+      return paddedString
+    }
+  }
+};
+
+// Testing:
+// console.log(_.clamp(1, 2, 4))
